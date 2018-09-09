57e5c046b7aa0c4bca356eb958ad9105d0b83617
Desarrollador <cristian.galeano1913@gmail.com>
2018-09-08 21:44:24 -0500

    Update Laravel and Storage with Drive

--- app/Traits/HasArchives.php
+++ app/Traits/HasArchives.php
@@ -10,12 +10,12 @@ use Illuminate\Support\Facades\Storage;
 trait HasArchives {
 
 //En algunos casos puede que se necesite setear esta propiedad para guardar los archivos en otro lugar
-	public function setDestino($destino){
-		$this->destino = $destino;
-	}
+  public function setDestino($destino){
+    $this->destino = $destino;
+  }
 
 //Relación polimórfica al model Archivos.
-	public function archives(){
+  public function archives(){
       return $this->morphMany(\App\Models\Archive::class, 'archivable');
   }
 
@@ -32,22 +32,26 @@ trait HasArchives {
 
    public function saveArchive($archive){
 
-    $path = 'uploads/' . $this->setDestinoArchivos();
+      if ($this->disk() == 'public') {
+        $path = 'uploads/' . $this->setDestinoArchivos();
 
-    $name = time() . '_' . $archive->getClientOriginalName();
+        $name = time() . '_' . $archive->getClientOriginalName();
 
         $upload = Storage::url(
-            $archive->storeAs(
-              $path, 
-              $name,
-              $this->disk()
-             )
-          );
+          $archive->storeAs(
+            $path, 
+            $name,
+            $this->disk()
+          )
+        );
 
         $this->archives()->create([
           'route' => 'uploads/' . $this->setDestinoArchivos() . '/' . $name,
           'name' => $archive->getClientOriginalName()
         ]);
+      } else if($this->disk() == 'drive'){
+        $this->saveInDrive($archive);
+      }
 
    }
 
@@ -62,14 +66,40 @@ trait HasArchives {
     if(method_exists($this, 'getNumeroDocumentoPaciente')){
       $destino .= '/' . $this->getNumeroDocumentoPaciente();
     }
-
-    
-
     return $destino;
    }
 
    private function disk(){
-    return 'public';
+      return 'drive';
+   }
+
+
+   private function saveInDrive($file){
+    $archive = $file;
+    $filename = Storage::cloud()->put('', $file);
+    // Get the file to find the ID
+    $dir = '/';
+    $recursive = false; // Get subdirectories also?
+    $contents = collect(Storage::cloud()->listContents($dir, $recursive));
+    $file = $contents
+          ->where('type', '=', 'file')
+          ->where('filename', '=', pathinfo($filename, PATHINFO_FILENAME))
+          ->where('extension', '=', pathinfo($filename, PATHINFO_EXTENSION))
+          ->first(); // there can be duplicate file names!
+    // Change permissions
+    // - https://developers.google.com/drive/v3/web/about-permissions
+    // - https://developers.google.com/drive/v3/reference/permissions
+      $service = Storage::cloud()->getAdapter()->getService();
+      $permission = new \Google_Service_Drive_Permission();
+      $permission->setRole('reader');
+      $permission->setType('anyone');
+      $permission->setAllowFileDiscovery(false);
+      $permissions = $service->permissions->create($file['basename'], $permission);
+
+      $this->archives()->create([
+          'route' => Storage::cloud()->url($file['path']),
+          'name' => $archive->getClientOriginalName()
+      ]);
    }
    
 }
